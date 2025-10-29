9a0cdd38eab5c7276068b3861fedada2d1d8f777
Rémi FERMENT <ferment.remi.pro@gmail.com>
2025-10-29 17:57:18 +0100

    Add Recipes Controllers.

--- NutriLink.API/Controllers/RecipesController.cs
+++ NutriLink.API/Controllers/RecipesController.cs
@@ -6,7 +6,7 @@ namespace NutriLink.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class RecipesController: ControllerBase
+    public class RecipesController : ControllerBase
     {
         private readonly AppDbContext _db;
         public RecipesController(AppDbContext db)
@@ -18,6 +18,59 @@ namespace NutriLink.API.Controllers
         {
             var recipes = await _db.Recipes.Include(r => r.Category).ToListAsync();
             return Ok(recipes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetById(int id)
+        {
+            var recipe = await _db.Recipes.Include(r => r.Category).FirstOrDefaultAsync(r => r.Id == id);
+
+            if (recipe == null) return NotFound(new { message = $"Recipe with ID {id} not found." });
+
+            return Ok(recipe);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> Create([FromBody] Recipe recipe)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var categoryExist = await _db.Categories.AnyAsync(c => c.Id == recipe.CategoryId);
+            if (!categoryExist) return BadRequest(new { message = "Invalid Category" });
+
+            _db.Add(recipe);
+            await _db.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = recipe.Id }, recipe);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update(int id, [FromBody] Recipe input)
+        {
+            if (id != input.Id) return BadRequest(new { message = "ID in route and body don't match." });
+
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var recipe = await _db.Recipes.FindAsync(id);
+            if (recipe == null) return NotFound();
+
+            recipe.Title = input.Title;
+            recipe.Steps = input.Steps;
+            recipe.Category = input.Category;
+
+            await _db.SaveChangesAsync();
+            return Ok(recipe);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var recipe = await _db.Recipes.FindAsync(id);
+            if (recipe == null) return NotFound();
+
+            _db.Remove(recipe);
+            await _db.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
